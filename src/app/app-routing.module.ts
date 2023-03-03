7b5dc323b3c4084c156a00ecd3979d1dd6b4f799
Abdallah <bratosab@gmail.com>
2023-03-03 11:58:17 +0100

    feat(click travel): Added click travel app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DestinationsComponent } from './destinations/destinations.component';
+import { TicketsComponent } from './tickets/tickets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DestinationsComponent },
+  { path: 'tickets/:code', component: TicketsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TicketsComponent } from './tickets/tickets.component';
+import { DestinationsComponent } from './destinations/destinations.component';
+import { LogoComponent } from './logo/logo.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedMaterialModule } from './shared-material/shared-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    DestinationsComponent,
+    TicketsComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
